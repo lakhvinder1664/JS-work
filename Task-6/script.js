9f12565409774e0a7257a686b7ad24c0b38b3a05
lakhvinder1664 <lakhvinder9567@gmail.com>
2025-07-31 13:32:56 +0530

    JS-work

---
+++ Task-6/script.js
@@ -0,0 +1,24 @@
+// Type conversion
+
+
+// let a = "name", b = 19;
+// console.log(typeof a, typeof b);
+
+
+// String Conversion
+
+// example-1
+// let a_10 = 19;
+// a_10 = String(a_10);
+
+// console.log(typeof a_10);
+
+// example-2
+let a_10 = 19;
+let b_10 = 20;
+
+a_10 = String(a_10);
+b_10 = String(b_10);
+
+console.log(typeof a_10, typeof b_10);
+ 
