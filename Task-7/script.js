ce496aaea38b8c7bbddf38c97965685e8c9eff2d
lakhvinder1664 <lakhvinder9567@gmail.com>
2025-08-01 00:24:41 +0530

    JS-work

---
+++ Task-7/script.js
@@ -0,0 +1,35 @@
+//Basic Operators, Math
+
+// Unary
+// let lucky = 19;
+// lucky = -lucky;
+// console.log(lucky);
+
+// example-2
+//  let num = 9;
+//  secnum = 10;
+
+//example-3
+//  console.log(num / secnum);
+// let num = 3;
+// bum = 8;
+// console.log(num % bum);
+
+
+
+// Remainder
+// let num = 4;
+// tick = 9;
+// console.log(num % tick)
+
+// Exponentation
+// let num = 7;
+// another = 8;
+// console.log(num ** another);
+
+// String concatenation with binary +
+
+// let s = "my" + "string";
+// console.log(s);
+
+console.log("1" + 2);
---
+++ Type conversion task/script.js
@@ -0,0 +1,115 @@
+// 1. What will be the output and type of this code?
+//  let x = "456";
+//  x = Number(x);
+//  console.log(x);
+//  console.log(typeof x);
+
+// let x = "456";
+// x = Number(x);
+// console.log(x);
+// console.log(typeof x);
+
+// What is the result of converting an empty string "" to a boolean?
+
+// let a = ""
+// a = Boolean(a);
+// console.log(typeof a);
+// false
+
+//  3. Will Boolean("false") return true or false ?
+
+// let a = "false";
+// a = Boolean(a);
+// console.log(a);
+// console.log(typeof a);
+// True
+
+// 4. What will typeof NaN return?
+// let result = typeof NaN;
+// console.log(result);
+// console.log(typeof result);
+
+    // let a = "abc" - 5;
+    // console.log(a);
+    // Number
+
+//  5. What happens when you do this:
+//  let value = undefined;
+//  value = Number(value);
+//  console.log(value);
+//  Conversion Understanding
+
+// let value;
+// value = Number(value);
+// console.log(value);
+
+//  6. Convert the following values into Boolean and Number, and write their types:- 
+// "0"
+// "hello"
+
+// 0 to Boolean:
+// let value = "0";
+// value = Boolean(value);
+// console.log(value);
+// console.log(typeof value);
+
+// 0 to Number:
+// let value = "0";
+// value = Number(value);
+// console.log(value);
+// console.log(typeof value);
+
+// hello to Boolean:
+// let str = "hello";
+// str = Boolean(str);
+// console.log(str);
+// console.log(typeof str);
+
+// hello to Number:
+// let str = "hello";
+// str = Number(str);
+// console.log(str);
+// console.log(typeof str);
+
+// // null to Boolean:
+// let a = null;
+// a = Boolean(a);
+// console.log(a);
+// console.log(typeof a);
+
+// null to Number:
+// let a = null;
+// a = Number(a);
+// console.log(a);
+// console.log(typeof a);
+
+// undefined to Boolean:
+// let a = undefined;
+// a = Boolean(a);
+// console.log(a);
+// console.log(typeof a);
+
+
+// undefined to Number:
+// let a = undefined;
+// a = Number(a);
+// console.log(a);
+// console.log(typeof a);
+
+// //  7. Predict the result:
+// //  let a = "42px";
+// //  let b = Number(a);
+// //  console.log(b);
+
+// let a = "42px";
+// let b = Number(a);
+// console.log(b);
+
+//  10. What is the result of:
+//  let n = " ";
+//  n = Boolean(n);
+//  console.log(n)
+
+// let a = " ";
+// a = Boolean(a);
+// console.log(a) 
