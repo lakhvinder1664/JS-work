148536ad4158e3e0b12417cc9f374eb44e721de5
lakhvinder1664 <lakhvinder9567@gmail.com>
2025-08-06 13:48:07 +0530

    JS-work

---
+++ Task-10/script.js
@@ -0,0 +1,53 @@
+// Loops
+
+// while (condition) {
+
+// }
+
+// let i = 0;
+// while (i < 3) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// let b = 10;
+// while (b = 20){
+//     console.log(b);
+//    b++;
+// }
+
+// let a = 15;
+// while (a <= 45){
+//     console.log(a);
+//     a++;
+// }
+
+// let l = 0;
+// while (l<=1000){
+// if(l % 5 == 0){
+//   console.log(l);
+// }
+//  l++;
+// }
+
+
+// 0 to 10
+// let a = 0;
+// while(a <= 10){
+//     console.log(a);
+//     a++;
+// }
+
+// even no. to 1 to 20
+let l = 1;
+do{
+
+    if(l % 2 == 0){
+        console.log(l);
+    }
+    l++;
+}while(l <= 20)
+// Print odd numbers from 15 to 1 (reverse order)
+// Output: 15 13 11 9 7 5 3 1
+
