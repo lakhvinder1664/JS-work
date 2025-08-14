849e47c8d435254ce67f88203d26014ef9460b4b
lakhvinder1664 <lakhvinder9567@gmail.com>
2025-08-14 09:47:50 +0530

    Js-work

---
+++ Task of Array/script.js
@@ -0,0 +1,21 @@
+let fruits_3 = ["apple", "grapes", "banana"];
+
+// PRINT VALUES
+// console.log(fruits_3);
+// console.log(fruits_3[2]);
+// console.log(fruits_3.length);
+
+// ADD OR REMOVE
+// fruits_3.push("orange");
+// fruits_3.pop();
+// fruits_3.unshift("grapes");
+// fruits_3.shift();
+// console.log(fruits_3);
+
+
+// USING WHILE LOOP
+let i = 0;
+while (i < fruits_3.length) {
+    console.log(fruits_3[i]);
+    i++;
+}
