830cb23d2ead68b367bef0b9c4322f95ac015a2b
lakhvinder1664 <luckysngh08966@gmail.com>
2025-09-06 22:49:00 +0530

    Js-work

--- Task-15/index.js
+++ Task-15/index.js
@@ -315,9 +315,9 @@ const temperature = 41
 const userEmail = []
 
 if (userEmail) {
-    console.log("got email");
+    // console.log("got email");
 } else {
-    console.log("didn't get it");
+    // console.log("didn't get it");
 }
 
 
@@ -337,15 +337,56 @@ if (userEmail) {
 //     console.log("objec is empty");
 // }
 
-let val1;
+// let val1;
 // val1 = 5 ?? 10
 // val1 = null ?? 10
 // val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+// val1 = null ?? 10 ?? 20
 
-console.log(val1);
+// console.log(val1);
 
 // condition ? true : false
 
-const iceTeaPrice = 100
-iceTeaPrice <=80 ? console.log("lesss than 80") : console.log("more than 80")
+// const iceTeaPrice = 100
+// iceTeaPrice <=80 ? console.log("lesss than 80") : console.log("more than 80")
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5){
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+
+}
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//     // console.log(`Inner loop value ${j} and inner loop ${i}`)
+//     console.log(i + '*' + j + ' = ' + i * j);
+//     }
+// }
+
+let myArray = ["Spiderman", "Superman", "Zoro"]
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+// for (let index = 1; index  <= 20; index++) {
+//     if(index == 5){
+//         console.log(`Detected 5`);
+//         break;
+//     }
+//     console.log(`Value of i is ${index}`);
+    
+// }
+for (let index = 1; index  <= 20; index++) {
+    if(index == 5){
+        console.log(`Detected 5`);
+        continue;
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
